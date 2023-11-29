fd6135647d486cbdcce79153765948b5579b5dd1
EmpireAntz <thebigboognish@gmail.com>
2023-11-28 20:59:37 -0800

    added quality comments throughout the project.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+//Imports inquirer
 const inquirer = require('inquirer')
+//File systems
 const fs = require('fs')
+//Shapes module with circle triangle and square classes
 const { Circle, Triangle, Square } = require('./lib/shapes.js')
-
+//Main function for the logo generation 
 async function main () {
+    //Uses inquirer to present the user with prompts for input
     const answers = await inquirer.prompt([
         {
             type: 'input',
@@ -27,26 +31,30 @@ async function main () {
             message: 'Choose the shapes color(keywords or hexidecimal value)',
         },
     ])
-
+    //Shape choices maps the shapes choices from our prompts to their respective classes
     const shapeChoices = {
         Triangle: Triangle,
         Circle: Circle,
         Square: Square,
     }
-
+    //Selects shape based on user input
     const ShapeClass = shapeChoices[answers.shape]
+    //Instantiate the selected shape with the selected color
     const shape = new ShapeClass(answers.shapeColor)
-
+    //Constructs the SVG logo
     const svgLogo = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     ${shape.render()}
     <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${answers.textColor}" font-size="50">${answers.text}</text>
     </svg>`
-
+    //Writes the SVG content to an svg file to see the logo
     fs.writeFile('./examples/logo.svg', svgLogo, (err) => {
         if (err) {
+            //Logs any errors to the console
             console.error(err)
         }
+        //Confirmation message on successfully written file
         console.log("Generated logo.svg")
     })
 }
+//Calls the main function
 main()
